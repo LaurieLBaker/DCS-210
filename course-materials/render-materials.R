1a295960cfddf7739cee9dc22d23a186245a4aeb
LaurieLBaker <laurie.baker@ons.gov.uk>
2023-04-21 19:34:09 -0400

    Add acknowledgements for next slides

--- course-materials/render-materials.R
+++ course-materials/render-materials.R
@@ -10,14 +10,26 @@ library(Tmisc)
 
 # non-xaringan -----------------------------------------------------------------
 
-rmds <- dir_info(recurse = 3, glob = "course-materials/hw-instructions/hw-01/*.Rmd") %>% 
+rmds <- dir_info(recurse = 3, glob = "course-materials/hw-instructions/hw-07/*.Rmd") %>% 
   filter(!str_detect(path, "slides")) %>%
   pull(path)
 walk(rmds, render)
 
 # xaringan ---------------------------------------------------------------------
 
-xaringans <- dir_info(recurse = 3, glob = "course-materials/slides/u4-d01-language-of-models/*.Rmd") %>% 
+xaringans <- dir_info(recurse = 3, glob = "course-materials/slides/u4-d06-logistic-reg/*.Rmd") %>% 
+  filter(str_detect(path, "slides")) %>%
+  filter(!str_detect(path, "setup")) %>%
+  pull(path)
+walk(xaringans, render)
+
+xaringans <- dir_info(recurse = 3, glob = "course-materials/slides/u4-d07-prediction-overfitting/*.Rmd") %>% 
+  filter(str_detect(path, "slides")) %>%
+  filter(!str_detect(path, "setup")) %>%
+  pull(path)
+walk(xaringans, render)
+
+xaringans <- dir_info(recurse = 3, glob = "course-materials/slides/u4-d08-feature-engineering/*.Rmd") %>% 
   filter(str_detect(path, "slides")) %>%
   filter(!str_detect(path, "setup")) %>%
   pull(path)
