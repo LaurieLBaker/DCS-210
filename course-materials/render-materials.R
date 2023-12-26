87386338943deb742f97b67c6e78d6fcb24be6e7
Laurie Baker <lbaker@coa.edu>
2023-12-26 14:28:24 -0500

    Add prelim stringr slides

--- course-materials/render-materials.R
+++ course-materials/render-materials.R
@@ -10,15 +10,14 @@ library(Tmisc)
 
 # non-xaringan -----------------------------------------------------------------
 
-rmds <- dir_info(recurse = 3, glob = "course-materials/hw-instructions/hw-08/*.Rmd") %>% 
+rmds <- dir_info(recurse = 3, glob = "course-materials/hw-instructions/hw-04/*.Rmd") %>% 
   filter(!str_detect(path, "slides")) %>%
   pull(path)
 walk(rmds, render)
 
 # xaringan ---------------------------------------------------------------------
 
-
-xaringans <- dir_info(recurse = 3, glob = "course-materials/slides/u4-d09-cross-validation/*.Rmd") %>% 
+xaringans <- dir_info(recurse = 3, glob = "course-materials/slides/u5-d08-stringr-regex/*.Rmd") %>% 
   filter(str_detect(path, "slides")) %>%
   filter(!str_detect(path, "setup")) %>%
   pull(path)
