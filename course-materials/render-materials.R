6ea779ee438eb0b96a3ca417fafbbc922dc3cc80
LaurieLBaker <lbaker@bates.edu>
2025-10-10 12:00:38 -0400

    Make sure pencils show up in HW 3 instructions

--- course-materials/render-materials.R
+++ course-materials/render-materials.R
@@ -11,20 +11,14 @@ library(dsbox)
 
 # non-xaringan -----------------------------------------------------------------
 
-rmds <- dir_info(recurse = 3, glob = "course-materials/lab-instructions/lab-01/lab-01-hello-r-2.Rmd") %>% 
-  filter(!str_detect(path, "slides")) %>%
-  pull(path)
-walk(rmds, render)
-
-
-rmds <- dir_info(recurse = 3, glob = "course-materials/hw-instructions/hw-02/hw-02-airbnb-edi.Rmd") %>% 
+rmds <- dir_info(recurse = 3, glob = "course-materials/hw-instructions/hw-01/hw-01-pet-names.Rmd") %>% 
   filter(!str_detect(path, "slides")) %>%
   pull(path)
 walk(rmds, render)
 
 # xaringan ---------------------------------------------------------------------
 
-xaringans <- dir_info(recurse = 3, glob = "course-materials/slides/u2-d11-data-classes/*.Rmd") %>% 
+xaringans <- dir_info(recurse = 3, glob = "course-materials/slides/u2-d04-viz-cat/*.Rmd") %>% 
   filter(str_detect(path, "slides")) %>%
   filter(!str_detect(path, "setup")) %>%
   pull(path)
