d3347fd178f32cd07e55699406697dab2b06e197
LaurieLBaker <lbaker@bates.edu>
2025-11-09 13:51:59 -0500

    Update Annotation Slides

--- course-materials/render-materials.R
+++ course-materials/render-materials.R
@@ -18,7 +18,7 @@ walk(rmds, render)
 
 # xaringan ---------------------------------------------------------------------
 
-xaringans <- dir_info(recurse = 3, glob = "course-materials/slides/u2-d04-viz-cat/*.Rmd") %>% 
+xaringans <- dir_info(recurse = 3, glob = "course-materials/slides/u5-d10-axes-themes/*.Rmd") %>% 
   filter(str_detect(path, "slides")) %>%
   filter(!str_detect(path, "setup")) %>%
   pull(path)
