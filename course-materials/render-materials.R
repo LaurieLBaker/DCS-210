a79f86da160726243fb3f412666e847c6ac0b01c
LaurieLBaker <lbaker@bates.edu>
2026-09-03 08:45:10 -0400

    Update Welcome slides

--- course-materials/render-materials.R
+++ course-materials/render-materials.R
@@ -23,7 +23,7 @@ walk(rmds, render)
 
 # xaringan ---------------------------------------------------------------------
 
-xaringans <- dir_info(recurse = 3, glob = "course-materials/slides/u1-d02-toolkit-r/*.Rmd") %>% 
+xaringans <- dir_info(recurse = 3, glob = "course-materials/slides/u1-d01-welcome/*.Rmd") %>% 
   filter(str_detect(path, "slides")) %>%
   filter(!str_detect(path, "setup")) %>%
   pull(path)
